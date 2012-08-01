dec8a31187bac708bf6a3de3e5abcd38ba716d1a
christianr1337 <christian.richter1337@googlemail.com>
2012-08-01 17:44:22 +0200

    paar anpassungen

--- movieOmat/movieOmat/settings.py
+++ movieOmat/movieOmat/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/c1337b/workspace/movieOmat/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/c1337b/git/movieOmat/movieOmat/sqlite.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -115,13 +115,14 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
+    'movies'
 )
 
 # A sample logging configuration. The only tangible logging
